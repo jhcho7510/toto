048cf302096190f9c378f3e049a430ee8f49d5ba
jhcho <jhcho7510@gmail.com>
2022-09-11 00:26:58 +0900

    toto 최초생성

---
+++ src/main/java/com/example/toto/controller/TotoController.java
@@ -0,0 +1,14 @@
+package com.example.toto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TotoController {
+
+	@GetMapping("/toto")
+	public String toto() {
+		return "Toto Getin";
+	}
+}
+ 
