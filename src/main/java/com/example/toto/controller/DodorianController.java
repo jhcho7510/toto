cf915e749cc0623416eff8e086356c7256f07ca3
jhcho <jhcho7510@gmail.com>
2022-09-19 00:14:27 +0900

    웹페이지 크롤링 참조 정보 서칭

---
+++ src/main/java/com/example/toto/controller/DodorianController.java
@@ -0,0 +1,14 @@
+package com.example.toto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dodorian")
+public class DodorianStockController {
+
+    @RequestMapping("/stock")
+    public void dodorianTest() {
+        
+    }
+}
---
+++ src/main/java/com/example/toto/service/DodorianService.java
@@ -0,0 +1,2 @@
+package com.example.toto.service;public class DodorianService {
+}
