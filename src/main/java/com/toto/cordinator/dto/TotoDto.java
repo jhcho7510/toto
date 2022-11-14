40d1c0e638e4e07c7edf231d5a1be856c804a240
jhcho <jhcho7510@gmail.com>
2022-11-14 23:38:15 +0900

    미사용 의존성 제거

--- src/main/java/com/toto/cordinator/dto/TotoDto.java
+++ src/main/java/com/toto/cordinator/dto/TotoDto.java
@@ -1,26 +1,18 @@
 package com.toto.cordinator.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TotoDto {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	
 	@NotBlank(message = "이름은 필수 입력 입니다.") private String name;
 	@NotBlank(message = "EMAIL은 필수 입력 입니다.") private String email;
 	
